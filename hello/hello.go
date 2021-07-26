e5d4aa741bbb1acf65d000bd3570c500cc670542
Brian Nolan <bp.nolan08@gmail.com>
2021-07-25 20:46:15 -0400

    go tutorial

---
+++ hello/hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+	"rsc.io/quote"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Gladys")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+	fmt.Println(quote.Go())
+}
